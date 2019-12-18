05523200068b1e1f56c1a8396f0c8515b41480bf
Eddy Subratha <eddy.subratha@gmail.com>
2019-12-18 22:20:07 +0700

    New upload

---
+++ tests/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            $this->app->version(), $this->response->getContent()
+        );
+    }
+
+    public function testTrue()
+    {
+        $this->get('/test');
+
+        $this->assertTrue(true, $this->response->getContent());
+    }
+}
